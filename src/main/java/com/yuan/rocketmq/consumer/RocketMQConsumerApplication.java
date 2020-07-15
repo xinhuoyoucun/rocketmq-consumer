41a0529a771aa64b9a2d6647c3d728f115bba0b0
laiyuan <417498119@qq.com>
2020-07-15 08:44:42 +0800

    init

---
+++ src/main/java/com/yuan/rocketmq/consumer/RocketMQConsumerApplication.java
@@ -0,0 +1,18 @@
+package com.yuan.rocketmq.consumer;
+
+import org.springframework.boot.Banner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author by yuanlai
+ * @Date 2020/7/14 4:14 下午
+ * @Description: TODO
+ * @Version 1.0
+ */
+@SpringBootApplication
+public class RocketMQConsumerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(RocketMQConsumerApplication.class,args);
+    }
+}
